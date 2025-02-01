54148fd3b36672c2efe55fc1ef3efef802783d64
LeandroSantosP <skp.tridimensional@gmail.com>
2025-01-31 23:12:08 -0300

    twosum

--- src/main/java/AlgoAndStructures/Leetcode/LeetCodeProblems.java
+++ src/main/java/AlgoAndStructures/Leetcode/LeetCodeProblems.java
@@ -1,12 +1,35 @@
 package AlgoAndStructures.Leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class LeetCodeProblems {
    public static void main(String[] args) {
       int[] s = { 1, 0, 0, 3, 4 };
       new LeetCodeProblems().moveZeroes(s);
+      System.out.println(Arrays.toString(s));
+   }
+   
+   //13
+
+   public int[] twoSum(int[] nums, int target) {
+      int[] result = new int[2];
+
+      HashMap<Integer, Integer> mrr = new HashMap<>();
+
+      for (int i = 0; i < nums.length; i++) {
+         var complement = target - nums[i]; 
+         if (mrr.containsKey(complement)) {
+            result[0] = i;
+            result[1] = mrr.get(complement);
+            break;
+         }
+         mrr.put(nums[i], i);
+      }
+
+      return result;
    }
 
    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
@@ -20,7 +43,7 @@ public class LeetCodeProblems {
 
       ArrayList<Boolean> kidsWithGrestestCaniies = new ArrayList<>();
 
-      for (int i = 0; i < candies.length; i++) {
+      for (int i = 0, l = candies.length; i < l; i++) {
          int crr = candies[i];
          if (crr + extraCandies >= max) {
             kidsWithGrestestCaniies.add(true);
@@ -35,12 +58,15 @@ public class LeetCodeProblems {
       // tracks where the next no-zero element should be placed.
       int last_no_zero_idx = 0;
       for (int num : nums) {
-         /* So, this part of the code shifts all non-zero elements to the front of the array. */
+         /*
+          * So, this part of the code shifts all non-zero elements to the front of the
+          * array.
+          */
          // if is an zero that no problem because if the next elements is an no-zero
          // than they will the replace by the next value
-         if (num!=0) {
+         if (num != 0) {
             nums[last_no_zero_idx] = num;
-            last_no_zero_idx++; 
+            last_no_zero_idx++;
          }
       }
 
